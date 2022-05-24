3f143962a54ce68b1ac62027e22aafbf74e913e6
Nyade <niinyade@icloud.com>
2022-05-24 17:18:18 -0300

    Started and finished the API and networking challenge with different structures, implementing MVC design pattern, JSON decoder, protocols and delegates, extensions and refactoring.

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+   
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        coinManager.delegate = self
+    }
+    
+//MARK: - UIPickerViewDelegate | UIPickerViewDataSource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
     }
 
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let currency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: currency)
+    }
 }
 
+//MARK: - CoinManagerDelegate
+
+extension ViewController: CoinManagerDelegate {
+    
+    func didUpdateBitCoin(_ coinManager: CoinManager, bitcoin: CoinModel) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = bitcoin.adjustedPrice
+            self.currencyLabel.text = bitcoin.currency
+        }
+    }
+
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,57 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateBitCoin(_ coinManager: CoinManager, bitcoin: CoinModel)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "5C16639A-F443-4BE9-8C27-C665E7429B4B"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
+    var delegate: CoinManagerDelegate?
+    
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let bitcoin = parseJSON(safeData){
+                        self.delegate?.didUpdateBitCoin(self, bitcoin: bitcoin)
+                    }
+                }
+            }
+            task.resume()
+            }
+        }
+    func parseJSON(_ coinData: Data) -> CoinModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: coinData)
+            let lastPrice = decodedData.rate
+            let actualCurrency = decodedData.asset_id_quote
+            
+            let bitcoin = CoinModel(price: lastPrice, currency: actualCurrency)
+            
+            return bitcoin
+            
+        } catch {
+            print(error)
+            return nil
+        }
+    }
     
 }
